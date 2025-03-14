922ba7fe0e4eaad1b7049f869bd44a07f0e14182
HaygiesJessica <sungahaygiesjessica@gmail.com>
2025-03-15 07:19:24 +0800

    Update models.py

--- expenses/models.py
+++ expenses/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils.timezone import now 
 
 class Expense(models.Model):
     CATEGORY_CHOICES = [
         ('Food', 'Food'),
         ('Transport', 'Transport'),
+        ('School', 'School'),
         ('Entertainment', 'Entertainment'),
         ('Health', 'Health'),
         ('Other', 'Other'),
@@ -16,3 +18,23 @@ class Expense(models.Model):
     
     def __str__(self):
         return f"{self.category} - ${self.amount}"
+
+
+class Budget(models.Model):
+    CATEGORY_CHOICES = [
+        ('Food', 'Food'),
+        ('Transport', 'Transport'),
+        ('School', 'School'),
+        ('Entertainment', 'Entertainment'),
+        ('Health', 'Health'),
+        ('Other', 'Other'),
+    ]
+
+    amount = models.FloatField()
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    description = models.TextField(blank=True, null=True)
+    date = models.DateField()
+    
+    def __str__(self):
+        return f"{self.category} - ${self.amount}"
+
