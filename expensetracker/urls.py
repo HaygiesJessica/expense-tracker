36aa0db926ab11895233600115720fcedda115ca
HaygiesJessica <sungahaygiesjessica@gmail.com>
2025-03-21 23:01:08 +0800

    Update urls.py

--- expensetracker/urls.py
+++ expensetracker/urls.py
@@ -2,12 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.http import JsonResponse
 
-# Simple welcome page for the root URL
-def home(request):
-    return JsonResponse({"message": "Welcome to the Expense Tracker API!"})
-
 urlpatterns = [
-    path('', home),  # Root URL
     path('admin/', admin.site.urls),
-    path('api/', include('expenses.urls')),  # API routes
+    path('api/', include('expenses.urls')),
 ]
